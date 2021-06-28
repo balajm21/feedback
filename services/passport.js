2c4367d39dd66757e357ad02ec6bfecbcb149741
Bala <m.elumalai777@gmail.com>
2021-06-28 22:59:53 +0530

    Added billing and client side app

--- services/passport.js
+++ services/passport.js
@@ -26,21 +26,15 @@ passport.use(
         callbackURL: '/auth/google/callback',
         Proxy: true
         }, 
-        (accessToken, refreshToken, profile, done) => {
+        async (accessToken, refreshToken, profile, done) => {
+            const existingUser = await User.findOne({googleId: profile.id})
+            
+            if (existingUser) {
+                return done(null, existingUser);
+            } 
 
-           User.findOne({googleId: profile.id})
-           .then((existingUser) => {
-               if (existingUser) {
-                   done(null, existingUser);
-        
-               } else {
-                new User({googleId: profile.id})
-                .save()
-                .then(user => done(null, user));
-
-               }
-           })
-
+            const user = await new User({googleId: profile.id}).save()
+            done(null, user);
         }
     )
 );
